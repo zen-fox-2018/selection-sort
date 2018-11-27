5df8c12841a1b8eb3d83e0d7f3371b7fac53462a
rachmadza <razato.zai@gmail.com>
2018-11-27 16:50:51 +0700

    push anchor selection sort

--- selection_sort.js
+++ selection_sort.js
@@ -1,1 +1,19 @@
 //your code here
+function sort(input) {
+    for (let i = 0; i < input.length; i++) {
+        var small = input[i]
+        for (let j = i + 1; j < input.length; j++) {
+            if (input[j] < small) {
+                var x = small
+                small = input[j]
+                input[j] = x
+            }
+        }
+        input[i] = small
+    }
+    return input
+}
+
+
+console.log(sort([33, 2, 52, 106, 73]));
+console.log(sort([13, 5, 22, 99, 11]));
