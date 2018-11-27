9b73f36236ae97aa7e90f1ae974a73598f83a6ee
Mahdihrs <mahdiharis@outlook.com>
2018-11-27 21:09:36 +0700

    sort menggunakan selection sort

--- selection_sort.js
+++ selection_sort.js
@@ -1,1 +1,20 @@
 //your code here
+function selectionSort(arr) {
+for (let i = 0; i < arr.length; i++) {
+  var indexGanti = i
+  for (let j = i + 1; j < arr.length; j++) {
+    if (arr[indexGanti] > arr[j]) {
+      debugger;
+      indexGanti = j
+    } 
+  }
+  var tmp = arr[i]
+  arr[i] = arr[indexGanti]
+  arr[indexGanti] = tmp 
+  }
+  return arr
+}
+
+console.log(selectionSort([5, 2, 7, 4, 21, 88, 3, 6, 90]))
+console.log(selectionSort([33, 2, 52, 106, 73]))
+console.log(selectionSort([13, 5, 22, 99, 11]))
