21524efaaa05a45f384fb4c1ed09eb26bfb622a2
kangman53 <hermanboy53@gmail.com>
2018-11-27 13:51:20 +0700

    selection sort done

--- selection_sort.js
+++ selection_sort.js
@@ -1,1 +1,37 @@
 //your code here
+// function selectionSort(arr){
+//   var min = 0;
+//   var indexMin = 0;
+//   for(var i = 0; i < arr.length; i++){
+//     min = arr[i];
+//     indexMin = i;
+//     for(var j = i + 1; j < arr.length; j++){
+//       if(arr[j] < min){
+//         min = arr[j];
+//         indexMin = j;
+//       }
+//     }
+//     arr[indexMin] = arr[i];
+//     arr[i] = min;
+//   }
+//   return arr;
+// }
+
+function selectionSort(arr){
+  var indexMin = 0;
+  for(var i = 0; i < arr.length; i++){
+    indexMin = i;
+    for(var j = i + 1; j < arr.length; j++){
+      if(arr[j] < arr[indexMin]){
+        indexMin = j;
+      }
+    }
+    var tempSwap = arr[indexMin];
+    arr[indexMin] = arr[i];
+    arr[i] = tempSwap;
+  }
+  return arr;
+}
+
+console.log(selectionSort([33, 2, 52, 106, 73]));
+console.log(selectionSort([13, 5, 22, 99, 11]));
