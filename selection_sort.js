f3ae6881d914c87a7d7d7c0e062b4fdf1d0d5051
Zhang <zhang@android-42520aa2424d51bc.localdomain>
2018-11-27 14:27:25 +0700

    done

--- selection_sort.js
+++ selection_sort.js
@@ -1,1 +1,24 @@
-//your code here
+function selection(input) {
+
+  for (let i = 0; i < input.length; i++) {
+    let temp = input[i]
+    var indexmin = i
+    // console.log(input)
+    for (let j = i+1 ; j < input.length; j++) { 
+      // console.log(j)
+      if (input[j] < input[indexmin]) {
+        // min = input[j]
+        indexmin = j
+      } 
+    }
+    //swap
+    if (i !== indexmin) {
+      input[i] = input[indexmin]
+      input[indexmin] = temp
+    }
+  }
+  return input
+}
+
+console.log(selection([ 33 , 2 , 52 , 106 , 73 ]))
+console.log(selection([ 13 , 5 , 22 , 99 , 11 ]))
