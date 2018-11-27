cb3c57c876107208247c8d22efeebaeaec29ab5d
ACER <fauziaazia171097@gmail.com>
2018-11-27 04:28:50 -0500

    finish

--- selection_sort.js
+++ selection_sort.js
@@ -1,1 +1,22 @@
 //your code here
+function selectionSort(arr){
+    for (let i = 0 ; i < arr.length; i++) {
+        let indexmin = i
+        let temp;
+        for (let j = i+1 ; j < arr.length; j++) {
+            if (arr[indexmin] > arr[j]) {
+                indexmin = j
+            }
+        }
+        if (i !== indexmin) {
+            temp = arr[i]
+            arr[i] = arr[indexmin]
+            arr[indexmin] = temp
+        }
+    }
+    return arr
+}
+
+
+let arr = [2,5,1,7,3,1,10]
+console.log(selectionSort(arr))
