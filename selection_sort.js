5214d70e48756d47ae8e6d4923e62feef5834103
Aditya Respati <AdityaRespati>
2018-11-27 18:08:39 +0700

    added solution

--- selection_sort.js
+++ selection_sort.js
@@ -1,1 +1,27 @@
 //your code here
+
+function selectionSort(arr){
+    
+    for(var i=0; i<arr.length-1; i++){
+        var min = arr[i]
+        var indexMin=i
+
+        for(var j=i; j<arr.length; j++){
+            if(min>arr[j]){
+                min=arr[j]
+                indexMin= j
+            }
+        }
+        
+        [arr[i], arr[indexMin]] = [arr[indexMin], arr[i]] 
+
+    }
+
+    return arr
+}
+
+console.log(selectionSort([33,2, 52, 106, 73]));
+
+console.log(selectionSort([13,5, 22, 99, 11]));
+
+
