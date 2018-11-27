6b15fc284b59db19a3a807d73bb84e2af5521527
maria <msmariaulfa33@gmail.com>
2018-11-27 18:42:06 +0700

    submit

--- selection_sort.js
+++ selection_sort.js
@@ -1,1 +1,22 @@
 //your code here
+function selectionSort(num){
+    for(i=0; i<num.length; i++){
+        var min = num[i]
+        var temp = i
+        for(j=i; j<num.length;j++){
+            if (num[j]< num[temp]){
+                var min = num[j]
+                temp = j
+            }
+        }
+        if(num[i] > min){
+            var tampung  = num[i]
+            num[i] = num[temp]
+            num[temp] = tampung
+        } 
+    }
+    return num
+}
+
+console.log(selectionSort([99, 33,2,52,106,73]))
+console.log(selectionSort([13,5,22,99,11]))
