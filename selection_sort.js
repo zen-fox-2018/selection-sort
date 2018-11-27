d1a468d049e297e07c9bff923b371c8142cdfcbd
fauzan_bintang <obin.aji@gmail.com>
2018-11-27 12:40:51 +0700

    fauzan

--- selection_sort.js
+++ selection_sort.js
@@ -1,1 +1,21 @@
-//your code here
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let min = arr[i]
+        let indexMin = i
+        for (var j = i+1; j < arr.length; j++) {
+            if (arr[j] < min) {
+                // console.log(arr);
+                min = arr[j]
+                indexMin = j
+                // console.log(min, indexMin);
+            }
+        }
+        arr[indexMin] = arr[i]
+        arr[i] = min
+        // console.log(arr);
+    }
+    return arr
+}
+
+console.log(selectionSort([33, 2, 52, 106, 73]));
+console.log(selectionSort([13, 5, 22, 99, 11]));
